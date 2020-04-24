e9f0fd5e700bf35ca03c0eac86f0420f6b1bc4f2
coder girl <56164343+kareey-commits@users.noreply.github.com>
2020-04-24 22:00:55 +0300

    Add files via upload

---
+++ primenumber.js
@@ -0,0 +1,82 @@
+// function isPrime(num){
+//     for(i===2;i<num;i++){
+//         if(num % i === 0){
+//             console.log("not a prime number");
+//         }
+//         else {
+//             console.log("this is a prime number");
+//         }
+//     }
+// }
+// console.log(isPrime(9));
+function isPrime(num){
+    if(num < 2){
+        return false;
+    }
+  for(let i = 2; i<num; i++){
+        if(num % i === 0){
+            return false;
+        }
+  }
+    return true;
+}
+console.log(isPrime(30));
+
+// let root =Math.ceil(Math.sqrt(num));
+// console.log(root);
+// for(let i = 2; i<num; i++){
+//             if(num % i == 0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+    // console.log(isPrime(30));
+// function isPrime(num){
+//    switch(true){
+//        case (num ==1):
+//            return false;
+//         case (num == 2):
+//             return true;
+//    }
+//     for(i =2; i<num; i++){
+//             if(num % i == 0){
+//                 return false;
+//             }
+//                 return true;
+// }
+// console.log(isPrime(37));
+// function isPrime(num){
+//     if(num == 1){
+//         console.log("Not a prime number");
+//     }else(num % 2 == 1)
+//     {
+//         console.log("This is a prime number");
+// }
+
+// }
+//  console.log(39);
+
+// function isPrime(num){
+//     switch(true){
+//         case (num == 1):
+//             return false;
+//             case ( num == 2):
+//                 return true;
+//     }
+//        for(num % i; i<num; i ++){
+//            math.floor(math.sqrt(num))
+//        }
+   
+//     }else{
+//          for(i =2; i< num; i++)
+//             if(num % i == 0){
+//                 return(false);
+//             }else{
+//                 return(true);
+//         } 
+//     }
+       
+// }
+// console.log(isPrime(37));
+
